4d57946f4429ea47b4a9e33bd99a5f059a486872
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-05-10 10:17:18 -0700

    turns out theres no sic code for google and facebook

--- fang.sql
+++ fang.sql
@@ -1,16 +1,19 @@
 -- Small tables for understanding joins
 
 CREATE TABLE fang_info AS
-SELECT * FROM company_info
+SELECT ticker, company_name, sic_code, net_income
+FROM company_info
 WHERE ticker IN ("FB", "AMZN", "NFLX", "GOOGL")
 ;
 
 CREATE TABLE fang_locations AS
-SELECT * FROM company_locations
+SELECT ticker, city, state, zip 
+FROM company_locations
 WHERE ticker IN (SELECT ticker FROM fang_info)
 ;
 
 CREATE TABLE fang_sic AS
 SELECT * FROM sic
-WHERE SIC IN (7841, 7370, 5961, 5182, 3292)
+WHERE SIC IN (SELECT sic_code FROM fang_info)
+OR SIC IN (5182, 3292)
 ;
